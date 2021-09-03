45578937da02605c27e3c0ca526140b73c37783c
Carlos Ortiz <carlospianiforte@gmail.com>
2021-09-03 00:38:04 -0500

    home

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css"
 import "./global.css"
 
-//import Badge from "./components/Badge"
-//import BadgeNew from "./pages/BadgeNew"
-import Badges from "./pages/Badges"
+
+import App from "./components/App"
 
 
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badges />, container);
+ReactDOM.render(<App />, container);
 
