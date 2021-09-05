19f284aae9467286689a04ec1313c768e70493ce
Carlos Ortiz <carlospianiforte@gmail.com>
2021-09-04 21:09:52 -0500

    LinkError

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Layout from "./Layout";
 
-//import Badge from "./components/Badge"
-import BadgeNew from "../pages/BadgeNew"
+import Layout from "./Layout";
+import Home from "../pages/Home";
 import Badges from "../pages/Badges"
+import BadgeNew from "../pages/BadgeNew"
 import NotFound from "../pages/NotFound"
 
 
@@ -13,6 +13,7 @@ function App(){
         <BrowserRouter>
             <Layout>
                 <Switch>
+                    <Route exact path="/" component={Home} />
                     <Route exact path="/badges" component={Badges} />
                     <Route exact path="/badges/new" component={BadgeNew} />
                     <Route component = {NotFound} />
@@ -27,4 +28,4 @@ function App(){
 
 }
 
-export default App
+export default App;
